50a10230215477565fc1f3f74b566249f693974e
brutchley <bhutchison0901@yahoo.com>
2023-10-28 14:09:04 -0500

    reeived API key from openweathermap.org and saved key as variable in js. Continuing to add css styling to keep webpage user friendly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
+var APIKey = "3d313856df69e730b9c4fe7a917b495a";
+
+
+
+
+
+//below function starts when search button is clicked, logging "it works!" in console to verify functionality, establishes form value as the city to be searched, and saves value in local storage
+
 document.getElementById("weather-button").addEventListener("click", function() {
     console.log("Click! It works :)")
+    let city = document.getElementById("city-weather").value;
 });
