b7ab5575559580cd80c4d678e088da4d9be95c58
brutchley <bhutchison0901@yahoo.com>
2023-10-28 19:11:16 -0500

    still working on history functions but may need help with it

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,25 @@
 var APIKey = "3d313856df69e730b9c4fe7a917b495a";
 var city = document.getElementById("city-weather");
-var storedCity = localStorage.getItem("city-name");
+var storedCity = localStorage.getItem(city.value);
 
 
-//you left off on this function below!!!!
+//this function is designed to take previous search value from search bar and save it as a new, created "p" tag within the article element
 
 function previousSearch() {
-    console.log(storedCity.value)
-    var lastSearch = []
+    console.log((storedCity));
+    var lastSearch = [];
     lastSearch.push($("#city-weather")).value;
     $.each(lastSearch), function(index, value) {
-        
+        const p = document.createElement("p");
+        p.innerHTML = value;
+        document.getElementById("previous-search").appendChild(p);
     }
 }
 
 //below function starts when search button is clicked, logging "it works!" in console to verify functionality, establishes form value as the city to be searched, and saves value in local storage
 
 document.getElementById("weather-button").addEventListener("click", function() {
-    console.log("Click! It works :)")
+    console.log("Click! It works :)");
     localStorage.setItem("city-name", city.value);
-    previousSearch()
+    previousSearch();
 });
