464f39090095d2b14844fadba0fe81f96b7cb262
brutchley <bhutchison0901@yahoo.com>
2023-10-29 12:12:58 -0500

    finally getting weatheer API to work in fetch function, getting data from a searched city and storing it in console, also saving last search in local strorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var APIKey = "3d313856df69e730b9c4fe7a917b495a";
 var city = document.getElementById("city-weather");
-var storedCity = localStorage.getItem(city.value);
+var requestURL = "https://api.openweathermap.org/data/2.5/weather?q={city}&appid=3d313856df69e730b9c4fe7a917b495a"
+var storedCity = localStorage.getItem("city-name", city.value);
 
 
 //this function is designed to take previous search value from search bar and save it as a new, created "p" tag within the article element
@@ -16,10 +17,31 @@ function previousSearch() {
     }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //below function starts when search button is clicked, logging "it works!" in console to verify functionality, establishes form value as the city to be searched, and saves value in local storage
 
 document.getElementById("weather-button").addEventListener("click", function() {
-    console.log("Click! It works :)");
     localStorage.setItem("city-name", city.value);
-    previousSearch();
+
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city.value + "&appid=3d313856df69e730b9c4fe7a917b495a")
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    });
+    //remember to call previousSearch() function here when it is figured out
 });
